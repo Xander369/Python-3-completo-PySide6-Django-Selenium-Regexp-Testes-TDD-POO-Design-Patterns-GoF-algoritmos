088dc692ec3cc92957aebd6c196997bb251af6c5
Xander369 <130623734+Xander369@users.noreply.github.com>
2025-05-01 18:22:57 -0300

    Commit

---
+++ aula1.py
@@ -0,0 +1,17 @@
+"""
+DocString.
+Escrever o que eu quiser. Por exemplo isso aqui
+
+
+"""
+'''DocString'''
+'''
+DocString.
+Usar para escrever um comentário
+
+'''
+#Permite escrever um comentário
+print(123) # Na frente (essa linha faz isso)
+# Abaixo 
+print(456)
+print("hello")
