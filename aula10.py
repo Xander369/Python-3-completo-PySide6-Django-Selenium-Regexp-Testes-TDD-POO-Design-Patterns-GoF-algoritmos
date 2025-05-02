3a2781ac1dcf7c65f146020822009dccb334a838
Xander369 <130623734+Xander369@users.noreply.github.com>
2025-05-02 12:01:55 -0300

    Teste

--- aula10.py
+++
@@ -1,7 +0,0 @@
-cocatenacao = 'Xander' + ' ' + 'Ricardo'
-print(cocatenacao)
-
-a_dez_vezes = 'A' * 10
-tres_vezes_xander = 3 * 'Xander'
-print(a_dez_vezes)
-print(tres_vezes_xander)
--- aula2.py
+++
@@ -1,6 +0,0 @@
-print(56, 78, sep='-', end='#')
-print(9, 10, sep='-', end='\n')
-print(9, 10, sep='-', end='\n')
-# \r\n -> CRLF
-# \n -> LF
-# Print não existe no Python
